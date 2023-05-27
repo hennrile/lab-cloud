218f81b7a63d51bc44c777e59e1c8c86624cdc89
hennrile <118722539+hennrile@users.noreply.github.com>
2023-05-27 12:03:10 +0700

    Add files via upload

---
+++ work1.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+fs.readFile('file1.txt', (err, data1) => {
+  if (err) throw err;
+
+  fs.readFile('file2.txt', (err, data2) => {
+    if (err) throw err;
+
+    fs.writeFile('file3.txt', data1 + data2, (err) => {
+      if (err) throw err;
+
+      console.log('Đã ghi nội dung của 2 file vào file3.txt');
+
+      fs.readFile('file3.txt', (err, data3) => {
+        if (err) throw err;
+
+        console.log('Nội dung của file3.txt là:');
+        console.log(data3.toString());
+      });
+    });
+  });
+});
