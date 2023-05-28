f752cf6135808a066a85e75891e8feff3227027e
HQHuy <huynguyen7770@gmail.com>
2023-05-28 15:35:31 +0700

    python project update

--- test_git.py
+++ test_git.py
@@ -21,20 +21,28 @@
 import subprocess
 from pexpect import popen_spawn
 
+
 user = 'HQHuy'
 password = '2345Nbvc'
 
 cmd = "cd C:\\Users\\TGDD\\Documents\\GitHub\\lad-cloud\\data\\test_github.txt"
+returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+
+cmd = "git add ." 
 subprocess.call(cmd, shell=True)
 
-subprocess.call("git add .", shell=True)
-subprocess.call('git commit -m "python project update"', shell=True)
-subprocess.call("git remote set-url origin hennrile/lab-cloud", shell=True)
+cmd = 'git commit -m "python project update"'
+subprocess.call(cmd, shell=True)
 
-child_process = popen_spawn.PopenSpawn("git push")
+cmd = "git remote set-url origin https://github.com/hennrile/lab-cloud"
+subprocess.call(cmd, shell=True)
+
+cmd = "git push "
+child_process = popen_spawn.PopenSpawn(cmd)
 child_process.expect('User')
 child_process.sendline(user)
 child_process.expect('Password')
 child_process.sendline(password)
+print('returned value:', returned_value)
 
 print('end of commands')
