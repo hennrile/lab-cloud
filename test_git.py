35db7071506c3a2628a6bb8c05a015a4f7592786
HQHuy <huynguyen7770@gmail.com>
2023-05-28 15:26:02 +0700

    python project update

--- test_git.py
+++ test_git.py
@@ -1,11 +1,32 @@
+# from github import Github
+# #asscess token
+# g = Github("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")
+# #Lấy thông tin repo
+# repo = g.get_repo("HQHuy/Learn-IT")
+
+# #Create File
+# contents = "Nội Dung File"
+# path = "C:\\Users\\TGDD\\Documents\\GitHub\\lad-cloud\\data\\test_github.txt"
+# message = "Add data"
+# branch = "main"
+
+# # Update file to created
+# # contents = "Nội dung mới của file"
+# # path = "path/to/file.txt"
+# # message = "Cập nhật file"
+# # branch = "main"
+# file = repo.get_contents(path)
+# repo.update_file(file.path, message, contents, file.sha, branch=branch)
+
+
 import subprocess
 from pexpect import popen_spawn
 
 
-user = input("Username: ")
-password = input("Password: ")
+user = 'HQHuy'
+password = '2345Nbvc'
 
-cmd = "cd C:\\Users\TGDD\Documents\GitHub\lad-cloud\test_github.txt"
+cmd = "cd C:\\Users\\TGDD\\Documents\\GitHub\\lad-cloud\\data\\test_github.txt"
 returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
 
 cmd = "git add ." 
