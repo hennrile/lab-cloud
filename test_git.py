01f121f2d9d9b76aa3587cb12abbce5782d816dc
HQHuy <huynguyen7770@gmail.com>
2023-05-28 16:19:12 +0700

    python project update

--- test_git.py
+++ test_git.py
@@ -18,9 +18,37 @@
 # file = repo.get_contents(path)
 # repo.update_file(file.path, message, contents, file.sha, branch=branch)
 
+# import subprocess
+# from pexpect import popen_spawn
+
+
+# user = 'HQHuy'
+# password = '2345Nbvc'
+
+# cmd = "cd C:\\Users\\TGDD\\Documents\\GitHub\\lad-cloud\\data\\test_github.txt"
+# returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+
+# cmd = "git add ." 
+# subprocess.call(cmd, shell=True)
+
+# cmd = 'git commit -m "python project update"'
+# subprocess.call(cmd, shell=True)
+
+# cmd = "git remote set-url origin https://github.com/hennrile/lab-cloud"
+# subprocess.call(cmd, shell=True)
+
+# cmd = "git push "
+# child_process = popen_spawn.PopenSpawn(cmd)
+# child_process.expect('User')
+# child_process.sendline(user)
+# child_process.expect('Password')
+# child_process.sendline(password)
+# print('returned value:', returned_value)
+
+# print('end of commands')
+
 import subprocess
 from pexpect import popen_spawn
-
 
 user = 'HQHuy'
 password = '2345Nbvc'
@@ -39,10 +67,14 @@ subprocess.call(cmd, shell=True)
 
 cmd = "git push "
 child_process = popen_spawn.PopenSpawn(cmd)
-child_process.expect('User')
-child_process.sendline(user)
-child_process.expect('Password')
-child_process.sendline(password)
+index = child_process.expect(['User', pexpect.EOF])
+if index == 0:
+    child_process.sendline(user)
+    child_process.expect('Password')
+    child_process.sendline(password)
+else:
+    print("EOF error occurred")
+
 print('returned value:', returned_value)
 
 print('end of commands')
